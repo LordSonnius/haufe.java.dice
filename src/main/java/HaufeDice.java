1375e2a2ff37fc55032785ed0cdb8c2a476e010a
Sebastian Adler <sebastian.adler@haufe-lexware.com>
2019-04-10 13:38:26 +0200

    Guess Update v. 1.1
    New Method added

--- src/main/java/HaufeDice.java
+++ src/main/java/HaufeDice.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class HaufeDice {
-
     public static void main(String[] args) {
         int n = 0;
         Dice wuerfel = new Dice(1);
@@ -12,8 +11,8 @@ public class HaufeDice {
             Scanner scan = new Scanner(System.in);
 
             System.out.print("Guesss the outcome.");//Ask user what he/she thinks is thrown
-
             int x = 0;
+
             while (x == 0) {
                 int num = scan.nextInt();
 
@@ -22,27 +21,11 @@ public class HaufeDice {
 
                 if (num < ocome) {
                     System.out.println("The outcome is higher than your guess. Do you want to guess again? 'y' or 'n'");
-                    String userinput = scan.next();
-
-                    if (userinput.equals("y")) {
-                        x = 0;
-                    } else if (userinput.equals("n")) {
-                        x = 1;
-                    } else {
-                        System.out.println("Error 404: Please type in 'y' or 'n'!");
-                    }
+                    x = guessquestion(scan, ocome);
 
                 } else if (num > ocome) {
                     System.out.println("The outcome is lower than your guess. Do you want to guess again? 'y' or 'n'");
-                    String userinput = scan.next();
-
-                    if (userinput.equals("y")) {
-                        x = 0;
-                    } else if (userinput.equals("n")) {
-                        x = 1;
-                    } else {
-                        System.out.println("Error 404: Please type in 'y' or 'n'!");
-                    }
+                    x = guessquestion(scan, ocome);
 
                 } else if (num == ocome) {
                     System.out.println("Congratulations, you guessed right :D ");
@@ -50,12 +33,6 @@ public class HaufeDice {
                 }
             }
 
-//
-//            //} catch (IOException e) {
-//            //   e.printStackTrace();
-//            //}
-//
-//
             System.out.print("Roll Dice again? Type 'y' for Yes and 'n' for No.");
 
 
@@ -74,5 +51,21 @@ public class HaufeDice {
         }
 
 
+    }
+
+    public static int guessquestion(Scanner scan, int ocome) {
+
+        int x = 0;
+        String userinput = scan.next();
+
+        if (userinput.equals("y")) {
+            x = 0;
+        } else if (userinput.equals("n")) {
+            System.out.println("The outcome is " + ocome);
+            x = 1;
+        } else {
+            System.out.println("Error 404: Please type in 'y' or 'n'!");
+        }
+        return x;
     }
 }
